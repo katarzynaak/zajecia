d15da39944dabcb42a71a2332238d26dbc4143d8
katarzynaak <mariannakaa@gmail.com>
2014-05-18 17:46:48 +0200

    dodanie funkcji getinfo

--- kalkulator.py
+++ kalkulator.py
@@ -3,5 +3,8 @@ def dodawanie():
     a = int(input('Wprowadz pierwsza liczbe'))
     b = int(input('Wprowadz druga liczbe'))
     print (a+b)
-
+def get_info():
+    print("ten program wywoluje")
+   
+get_info()
 dodawanie()
